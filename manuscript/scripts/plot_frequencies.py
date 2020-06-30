b9dbdabae8a0564cf785041d1bab7a04c120586c
Richard Neher <richard.neher@unibas.ch>
2020-07-01 00:39:00 +0200

    add colors and line styles to freq figure

--- manuscript/scripts/plot_frequencies.py
+++ manuscript/scripts/plot_frequencies.py
@@ -7,25 +7,27 @@ import matplotlib.pyplot as plt
 from scipy.stats import scoreatpercentile
 
 colors = {
-"19B": "#FF8080",
-"A": "#FF8080",
-"O": "#FFD938",
-"V": "#FFEEAA",
+"19B": {"color": "#FF8080", "ls":"-"},
+"A": {"color": "#FF8080", "ls":"-"},
 #
-"19A": "#FFB247",
-"B": "#FFB247",
-"L": "#FFB247",
+"19A": {"color": "#ffb247", "ls":"-"},
+"B": {"color": "#ffb247", "ls":"-"},
+"L": {"color": "#ffb247", "ls":"-"},
+"O": {"color": "#FFD938", "ls":"-"},
+"V": {"color": "#FFEEAA", "ls":"-"},
 #
-"20A": "#CDDE87",
-"G": "#CDDE87",
-"B.1": "#CDDE87",
+"20A": {"color": "#CDDE87", "ls":"-"},
+"G": {"color": "#CDDE87", "ls":"-"},
+"B.1": {"color": "#CDDE87", "ls":"-"},
+"B.1.5": {"color": "#CDDE87", "ls":"--"},
 #
-"20B": "#C6AFE9",
-"B.1.1": "#C6AFE9",
-"GR": "#C6AFE9",
+"20B": {"color": "#C6AFE9", "ls":"-"},
+"B.1.1": {"color": "#C6AFE9", "ls":"-"},
+"B.1.1.1": {"color": "#C6AFE9", "ls":"--"},
+"GR": {"color": "#C6AFE9", "ls":"-"},
 #
-"20C": "#AAEEFF",
-"GH": "#AAEEFF",
+"20C": {"color": "#AAEEFF", "ls":"-"},
+"GH": {"color": "#AAEEFF", "ls":"-"},
 }
 
 
@@ -92,13 +94,14 @@ for ax, attr in zip(axs, ["clade_membership", "GISAID_clade", "pangolin_lineage"
 
 
     for ci, clade in enumerate(sorted(frequencies.keys())):
-        if frequencies[clade].max() > 0.05:
-            col = colors[clade] if clade in colors else f'C{ci%10}'
+        if frequencies[clade].max() > 0.075:
+            col = colors[clade]["color"] if clade in colors else f'C{ci%10}'
+            ls = colors[clade]["ls"] if clade in colors else f'-'
             tmp_samples = np.array([(f[clade] if clade in f else np.zeros_like(num_date_points))
                                     for f in resampled_frequencies])
             upper = scoreatpercentile(tmp_samples, 75, axis=0)
             lower = scoreatpercentile(tmp_samples, 25, axis=0)
-            ax.plot(date_points, frequencies[clade], label=clade, lw=3, c=col)
+            ax.plot(date_points, frequencies[clade], label=clade, lw=3, c=col, ls=ls)
             ax.fill_between(date_points, lower, upper, color=col, alpha=0.3)
 
     ax.legend(fontsize=fs, ncol=3, loc=9)
